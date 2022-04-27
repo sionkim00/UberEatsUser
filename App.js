c5787e07fa5d642b99f8d7fe4fe2113dedf6b8b3
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-04-27 15:28:43 +0900

    Product cart screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import DishDetailScreen from "./src/screens/DishDetailScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailScreen from "./src/screens/RestaurantDetailScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailScreen />
+      <DishDetailScreen />
       <StatusBar style="light" />
     </View>
   );
